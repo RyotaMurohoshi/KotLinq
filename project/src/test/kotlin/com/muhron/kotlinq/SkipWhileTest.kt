803b9d21c09b9b64aea5653c1e3c3434955fc612
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-05 16:24:45 +0900

    added skip and skipWhile tests

---
+++ project/src/test/kotlin/com/muhron/kotlinq/SkipWhileTest.kt
@@ -0,0 +1,13 @@
+package com.muhron.kotlinq
+
+import org.junit.Assert
+import org.junit.Test
+
+class SkipWhileTest {
+
+    @Test
+    fun simple() {
+        val result = sequenceOf(1, 2, 3, 4, 5).skipWhile { it < 3 }.toList()
+        Assert.assertEquals(result, listOf(3, 4, 5))
+    }
+}
