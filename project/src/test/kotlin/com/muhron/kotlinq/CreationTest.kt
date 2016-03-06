5880066f1b4d0e023094f0e272a15d7da62eebf1
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-06 14:20:07 +0900

    added creation tests

---
+++ project/src/test/kotlin/com/muhron/kotlinq/CreationTest.kt
@@ -0,0 +1,47 @@
+package com.muhron.kotlinq
+
+import org.junit.Assert
+import org.junit.Test
+
+class CreationTest {
+
+    @Test
+    fun repeatTestCount5() {
+        val result = repeat("K", 5).toList();
+        Assert.assertEquals(result, listOf("K", "K", "K", "K", "K"));
+    }
+
+    @Test
+    fun repeatTestCount0() {
+        val result = repeat("K", 0).toList();
+        Assert.assertEquals(result, emptyList<String>());
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun repeatTestCountNegative() {
+        repeat("K", -1).toList();
+    }
+
+    @Test
+    fun rangeTestStart0Count5() {
+        val result = range(0, 5).toList();
+        Assert.assertEquals(result, listOf(0, 1, 2, 3, 4));
+    }
+
+    @Test
+    fun rangeTestStart3Count5() {
+        val result = range(3, 5).toList();
+        Assert.assertEquals(result, listOf(3, 4, 5, 6, 7));
+    }
+
+    @Test
+    fun rangeTestStart0Count0() {
+        val result = repeat(0, 0).toList();
+        Assert.assertEquals(result, emptyList<String>());
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun repeatTestStart0CountNegative() {
+        repeat(0, -1).toList();
+    }
+}
