02013a8c89f878418c580c1a5db0958c6f516650
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-08 00:31:25 +0900

    added thenBy test

---
+++ project/src/test/kotlin/com/muhron/kotlinq/ThenByTest.kt
@@ -0,0 +1,20 @@
+package com.muhron.kotlinq
+
+import org.junit.Assert
+import org.junit.Test
+
+class ThenByTest {
+
+    @Test
+    fun simple() {
+        val result = sequenceOf("Java", "Ceylon", "Xtend", "JRuby", "Kotlin", "Jython", "Groovy", "Scala", "Gosu")
+                .orderBy { it -> it.length }
+                .thenBy { it }
+                .toList()
+
+        Assert.assertEquals(
+                result,
+                listOf("Gosu", "Java", "JRuby", "Scala", "Xtend", "Ceylon", "Groovy", "Jython", "Kotlin")
+        )
+    }
+}
