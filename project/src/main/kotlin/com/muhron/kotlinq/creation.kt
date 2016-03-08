15cd25a98857696db0ed0dbf4a10cc5ab49d3fe4
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-08 21:17:15 +0900

    added join method

--- project/src/main/kotlin/com/muhron/kotlinq/creation.kt
+++ project/src/main/kotlin/com/muhron/kotlinq/creation.kt
@@ -1,5 +1,21 @@
 package com.muhron.kotlinq
 
+fun <TOuter, TInner, TKey, TResult> join(
+        outer: Sequence<TOuter>,
+        inner: Sequence<TInner>,
+        outerKeySelector: (TOuter) -> TKey,
+        innerKeySelector: (TInner) -> TKey,
+        resultSelector: (TOuter, TInner) -> TResult): Sequence<TResult> {
+    return Sequence {
+        val innerLookup = inner.toLookup(innerKeySelector)
+        outer.flatMap { outerElement ->
+            val key = outerKeySelector(outerElement)
+            val innerElements = innerLookup[key]
+
+            innerElements.map { innerElement -> resultSelector(outerElement, innerElement) }
+        }.iterator()
+    }
+}
 
 fun range(start: Int, count: Int): Sequence<Int> {
     require(count >= 0, { "count value must be positive or 0." })
