fb06a4263617ed1d3c4eb81580f933f54acd5e8c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-12 20:03:39 +0900

    deleted longCount test

--- src/test/kotlin/com/muhron/kotlinq/LongCountTest.kt
+++ src/test/kotlin/com/muhron/kotlinq/LongCountTest.kt
@@ -10,10 +10,4 @@ class LongCountTest {
         val result = sequenceOf(1, 2, 3, 4, 5).longCount()
         Assert.assertEquals(result, 5);
     }
-
-    @Test
-    fun simple1() {
-        val result = repeat(0, Int.MAX_VALUE).concat(sequenceOf(1)).longCount()
-        Assert.assertEquals(result, 1L + Int.MAX_VALUE);
-    }
 }
