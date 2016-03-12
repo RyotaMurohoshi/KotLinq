9703196ded3ed0bd49b964a045a8c35af56f32a3
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-12 15:51:10 +0900

    moved firstOrDefault and its test

--- src/main/kotlin/com/muhron/kotlinq/orDefault.kt
+++ src/main/kotlin/com/muhron/kotlinq/orDefault.kt
@@ -1,31 +1,5 @@
 package com.muhron.kotlinq
 
-// firstOrDefault
-fun <T> Sequence<T>.firstOrDefault(): T? =
-        firstOrNull()
-
-fun <T> Iterable<T>.firstOrDefault(): T? =
-        firstOrNull()
-
-fun <K, V> Map<K, V>.firstOrDefault(): Map.Entry<K, V>? =
-        asSequence().firstOrNull()
-
-fun <T> Array<T>.firstOrDefault(): T? =
-        firstOrNull()
-
-// firstOrDefault with predicate
-fun <T> Sequence<T>.firstOrDefault(predicate: (T) -> Boolean): T? =
-        firstOrNull(predicate)
-
-fun <T> Iterable<T>.firstOrDefault(predicate: (T) -> Boolean): T? =
-        firstOrNull(predicate)
-
-fun <K, V> Map<K, V>.firstOrDefault(predicate: (Map.Entry<K, V>) -> Boolean): Map.Entry<K, V>? =
-        asSequence().firstOrNull(predicate)
-
-fun <T> Array<T>.firstOrDefault(predicate: (T) -> Boolean): T? =
-        firstOrNull(predicate)
-
 // lastOrDefault
 fun <T> Sequence<T>.lastOrDefault(): T? =
         lastOrNull()
