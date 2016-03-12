19aaf03bb44d8d4ba6e25fcd04f7914c6cd47bf1
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-12 16:05:57 +0900

    moved lastOrDefault and added its test

--- src/main/kotlin/com/muhron/kotlinq/orDefault.kt
+++ src/main/kotlin/com/muhron/kotlinq/orDefault.kt
@@ -1,31 +1,5 @@
 package com.muhron.kotlinq
 
-// lastOrDefault
-fun <T> Sequence<T>.lastOrDefault(): T? =
-        lastOrNull()
-
-fun <T> Iterable<T>.lastOrDefault(): T? =
-        lastOrNull()
-
-fun <K, V> Map<K, V>.lastOrDefault(): Map.Entry<K, V>? =
-        asSequence().lastOrNull()
-
-fun <T> Array<T>.lastOrDefault(): T? =
-        lastOrNull()
-
-// lastOrDefault with predicate
-fun <T> Sequence<T>.lastOrDefault(predicate: (T) -> Boolean): T? =
-        lastOrNull(predicate)
-
-fun <T> Iterable<T>.lastOrDefault(predicate: (T) -> Boolean): T? =
-        lastOrNull(predicate)
-
-fun <K, V> Map<K, V>.lastOrDefault(predicate: (Map.Entry<K, V>) -> Boolean): Map.Entry<K, V>? =
-        asSequence().lastOrNull(predicate)
-
-fun <T> Array<T>.lastOrDefault(predicate: (T) -> Boolean): T? =
-        lastOrNull(predicate)
-
 // singleOrDefault
 fun <T> Sequence<T>.singleOrDefault(): T? =
         singleOrNull()
