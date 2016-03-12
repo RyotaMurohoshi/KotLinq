df0a56ec1ad90eac139398ad35a6b01b7979b911
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-12 15:06:51 +0900

    added count stub and its test

---
+++ src/main/kotlin/com/muhron/kotlinq/count.kt
@@ -0,0 +1,4 @@
+package com.muhron.kotlinq
+
+// count() and <T> count(predicate : (T) -> Boolean) methods are implemented in kotlin.collections or kotlin.sequences
+// The methods are same name and same parameter name.
