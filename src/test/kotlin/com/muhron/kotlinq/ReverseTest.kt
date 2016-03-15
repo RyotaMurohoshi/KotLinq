37b2c6e9b1901e5a2c9b7cc2291c225ab26aa42d
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-15 23:14:09 +0900

    fix reverse

--- src/test/kotlin/com/muhron/kotlinq/ReverseTest.kt
+++ src/test/kotlin/com/muhron/kotlinq/ReverseTest.kt
@@ -2,6 +2,7 @@ package com.muhron.kotlinq
 
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 
 class ReverseTest {
     @Test
@@ -14,5 +15,10 @@ class ReverseTest {
     fun test1() {
         val result = emptySequence<Int>().reverse().toList()
         Assert.assertEquals(result, emptyList<Int>())
+    }
+
+    @Test
+    fun testNoThrownException() {
+        exceptionSequence<Int>().reverse()
     }
 }
