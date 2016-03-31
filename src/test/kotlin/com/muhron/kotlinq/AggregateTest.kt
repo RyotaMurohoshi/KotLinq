980352960978a25a778096012a071010312ab4fa
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-31 23:18:00 +0900

    added empty test

--- src/test/kotlin/com/muhron/kotlinq/AggregateTest.kt
+++ src/test/kotlin/com/muhron/kotlinq/AggregateTest.kt
@@ -15,12 +15,32 @@ class AggregateTest {
     fun test1() {
         val result = range(1, 5).aggregate("", { a, b -> a + b })
 
-        Assert.assertEquals(result, "12345");
+        Assert.assertEquals(result, "12345")
     }
 
     @Test
     fun test2() {
         val result = range(1, 5).aggregate(0, { a, b -> a + b }, { it.toString() })
-        Assert.assertEquals(result, "15");
+
+        Assert.assertEquals(result, "15")
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun testEmpty0() {
+        emptySequence<Int>().aggregate { a, b -> a + b }
+    }
+
+    @Test
+    fun testEmpty1() {
+        val result = emptySequence<Int>().aggregate(0, { a, b -> a + b })
+
+        Assert.assertEquals(result, 0)
+    }
+
+    @Test
+    fun testEmpty2() {
+        val result = emptySequence<Int>().aggregate(0, { a, b -> a + b }, { it.toString() })
+
+        Assert.assertEquals(result, "0")
     }
 }
